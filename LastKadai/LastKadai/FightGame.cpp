ca785f52abec3aca6ec97621ebb211f462e576c0
ibuki-27 <KTC25A31A0050@edu.kyoto-tech.ac.jp>
2026-07-04 16:24:11 +0900

    save

---
+++ LastKadai/LastKadai/FightGame.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"FightGame.h"
+
+void Ally::swordsman()
+{
+
+}
+
+void Ally::wizard()
+{
+
+}
+
+void Ally::summoner()
+{
+
+}
---
+++ LastKadai/LastKadai/FightGame.h
@@ -0,0 +1,25 @@
+#pragma once
+
+class Ally
+{
+private:
+
+	void swordsman();
+
+	void wizard();
+
+	void summoner();
+
+};
+
+class Enemy
+{
+private:
+
+	void swordsman();
+
+	void wizard();
+
+	void summoner();
+
+};
